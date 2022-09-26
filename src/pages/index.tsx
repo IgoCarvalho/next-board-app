ad60d2442be544048d59eeec7cbf985194f91375
Igo Carvalho <igocarvalho00@gmail.com>
2022-09-26 10:35:07 -0300

    feat: initial app configuration

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Board - Organizando suas tarefas</title>
+      </Head>
+      <div >
+        <h1>Hello NextJs!</h1>
+      </div>
+    </>
+  )
+}
+
+export default Home
