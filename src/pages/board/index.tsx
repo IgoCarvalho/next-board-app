5542ffe4bcc9e9a266bf82b3ed5f1f00aed374d3
Igo Carvalho <igocarvalho00@gmail.com>
2022-09-26 16:44:25 -0300

    feat: create Board page

---
+++ src/pages/board/index.tsx
@@ -0,0 +1,63 @@
+import { FiCalendar, FiClock, FiEdit2, FiPlus, FiTrash2 } from 'react-icons/fi'
+
+import Head from 'next/head'
+
+import styles from './styles.module.scss'
+
+function Board() {
+  return (
+    <>
+      <Head>
+        <title>Minhas tarefas - Board</title>
+      </Head>
+      <main className={styles.boardContainer}>
+        <form>
+          <input type="text" placeholder="Qual sua tarefa?" />
+          <button type="submit">
+            <FiPlus size={25} />
+          </button>
+        </form>
+
+        <section className={styles.tasksList}>
+          <h1>Você tem 2 tarefas!</h1>
+
+          <article className={styles.task}>
+            <p>Aprender criar projetos usando Next JS e aplicando firebase como back.</p>
+
+            <div className={styles.taskInfo}>
+              <span>
+                <FiCalendar />
+                <time>17 de julho 2022</time>
+              </span>
+
+              <div className={styles.taskActions}>
+                <button>
+                  <FiEdit2 size={20} />
+                  Editar
+                </button>
+                <button>
+                  <FiTrash2 size={20} color={styles.dangerColor} />
+                  Excluir
+                </button>
+              </div>
+            </div>
+
+          </article>
+
+        </section>
+
+      </main>
+
+      <div className={styles.vipContainer}>
+        <h2>Obrigado por apoiar esse projeto.</h2>
+
+        <div>
+          <FiClock size={20} />
+          <span>Última doação cerca de 2 horas</span>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Board
